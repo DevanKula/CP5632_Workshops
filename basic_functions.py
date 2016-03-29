604ce2e044bac3172d4c9467cfd12b944440d6a1
Devan Kula <devan.kula@my.jcu.edu.au>
2016-03-29 15:22:58 +1000

    Updated program

--- basic_functions.py
+++ basic_functions.py
@@ -7,18 +7,22 @@ CP1404/CP5632 Workshop 04
 Basic functions
 demonstrating various parameters, returns and the use of a main function
 """
-__author__ = 'Lindsay Ward'
+__author__ = 'Devan Kula'
 
 
 def main():
     lowest, highest = get_limits()
-    print("lowest =", lowest, "highest =", highest)
-    print_between(lowest, highest)
+    while lowest > highest:
+        print("Maximum is too low!")
+        highest = int(input("Enter the maximum: ({} or above):".format(lowest)))
+    else:
+        print("lowest =", lowest, "highest =", highest)
+        print_between(lowest, highest)
 
 
 def get_limits():
     minimum = int(input("Enter the minimum: "))
-    maximum = int(input("Enter the maximum: "))
+    maximum = int(input("Enter the maximum: ({} or above):".format(minimum)))
     return minimum, maximum
 
 
