b5ef09c10dd3cdc9afe1a05ac4d716132861be4c
Devan Kula <devan.kula@my.jcu.edu.au>
2016-03-29 16:42:27 +1000

    updated stock price

--- stockprice.py
+++ stockprice.py
@@ -1,20 +1,28 @@
 import random
 
-MAX_INCREASE = 0.1  # 10%
-MAX_DECREASE = 0.05 # 5%
-MIN_PRICE = 0.01
-MAX_PRICE = 1000.0
-INITIAL_PRICE = 10.0
-day = 0
-price = INITIAL_PRICE
-print("Starting price: ${:,.2f}".format(price))
 
-while MIN_PRICE <= price <= MAX_PRICE:
-    priceChange = 0
-    day += 1
-    if random.randint(1, 2) == 1:
-        priceChange = random.uniform(0, MAX_INCREASE)
-    else:
-        priceChange =  random.uniform(-MAX_DECREASE, 0)
-    price *= (1 + priceChange)
-    print('On day {} price is ${:,.2f}'.format(day,price))
+def main():
+    MAX_INCREASE = 0.1  # 10%
+    MAX_DECREASE = 0.05 # 5%
+    MIN_PRICE = 1.0
+    MAX_PRICE = 100.0
+    INITIAL_PRICE = 10.0
+    day = 0
+    price = INITIAL_PRICE
+    print("Starting price", end=' ')
+    format_currency(price)
+
+    while MIN_PRICE <= price <= MAX_PRICE:
+        priceChange = 0
+        day += 1
+        if random.randint(1, 2) == 1:
+            priceChange = random.uniform(0, MAX_INCREASE)
+        else:
+            priceChange =  random.uniform(-MAX_DECREASE, 0)
+        price *= (1 + priceChange)
+        print("On day {} price is ".format(day), end=' ')
+        format_currency(price)
+
+def format_currency(value):
+    return print('${:,.2f}'.format(value))
+main()
