444724bfcdb3096d7064b65c988568e93410c59d
Devan Kula <devan.kula@my.jcu.edu.au>
2016-04-08 08:44:01 +1000

    updated stock price

---
+++ ASCII.py
@@ -0,0 +1,6 @@
+#lower = 10
+#upper = 100
+for i in range(10, 120, 11):
+    print("{}{}".format(i, chr(i)))
+#print("Enter a number ({} - {})".format(lower,upper))
+#input()
--- menu.py
+++ menu.py
@@ -1,17 +1,32 @@
+# name = input("Enter name: ")
+# print("(H)ello")
+# print("(G)oodbye")
+# print("(Q)uit")
+# choice = input().upper()
+# while choice != "Q":
+#     if choice == "H":
+#         print("Hello", name)
+#     elif choice == "G":
+#         print("Goodbye", name)
+#     else:
+#         print("Invalid choice")
+#     print("(H)ello")
+#     print("(G)oodbye")
+#     print("(Q)uit")
+#     choice = input().upper()
+# print("Finished")
+
 name = input("Enter name: ")
-print("(H)ello")
-print("(G)oodbye")
-print("(Q)uit")
-choice = input()
-while choice != "Q":
-    if choice == "H":
-        print("Hello", name)
-    elif choice == "G":
-        print("Goodbye", name)
+MENU = "(H)ello\n(G)oodbye\n(Q)uit  "
+print(MENU)
+choice = input(">>>").upper()
+while choice != 'Q':
+    if choice == 'H':
+        print("Hello {}".format(name))
+    elif choice == 'G':
+        print("Goodbye {}".format(name))
     else:
-        print("Invalid choice")
-    print("(H)ello")
-    print("(G)oodbye")
-    print("(Q)uit")
-    choice = input()
+        print("Invalid Choice")
+    print(MENU)
+    choice = input(">>>").upper()
 print("Finished")
