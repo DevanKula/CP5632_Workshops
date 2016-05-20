5d63146b0022eb038565c35085c06e1dfa008021
jc348087 <devan.kula@my.jcu.edu.au>
2016-05-20 14:18:17 +1000

    updated workshop 11

--- Workshop 9/ConvertertoMiles.py
+++ Workshop 9/ConvertertoMiles.py
@@ -1,12 +1,31 @@
 from kivy.app import App
 from kivy.lang import Builder
 
+CONVERSION = 1.60934
 
-class ConverttoMiles(App):
+
+class Convert_to_Miles(App):
     def build(self):
         self.title = "Convert Miles to Kilometers"
         self.root = Builder.load_file('ConvertertoMiles.kv')
         return self.root
 
-ConverttoMiles().run()
+    def calculate_conversion(self):
+        value = self.mile_test()
+        result = value * CONVERSION
+        self.root.ids.output_label.text = str(result)
+
+    def increment(self, change):
+        value = self.mile_test() + change
+        self.root.ids.input_miles.text = str(value)
+        self.calculate_conversion()
+
+    def mile_test(self):
+        try:
+            value = float(self.root.ids.input_miles.text)
+            return value
+        except ValueError:
+            return 0
+
+Convert_to_Miles().run()
 
